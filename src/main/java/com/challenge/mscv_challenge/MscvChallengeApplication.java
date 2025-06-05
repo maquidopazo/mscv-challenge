8416f4391200cca8103182aa2f5ac2ad96ed0154
Do Pazo, Cristina (Externo - Accenture) <cristina.m.do.pazo@accenture.com>
2025-06-05 20:09:56 -0300

    first commit

---
+++ src/main/java/com/challenge/mscv_challenge/MscvChallengeApplication.java
@@ -0,0 +1,15 @@
+package com.challenge.mscv_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscvChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscvChallengeApplication.class, args);
+
+		System.out.println("heloooooooo probando");
+	}
+
+}
---
+++ src/test/java/com/challenge/mscv_challenge/MscvChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.mscv_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscvChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
